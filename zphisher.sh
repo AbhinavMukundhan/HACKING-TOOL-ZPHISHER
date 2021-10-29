9eb981218de421a20f2820e1f7a888f34ba83dda
AbhinavMukundh <92980072+AbhinavMukundhan@users.noreply.github.com>
2021-10-29 12:32:04 +0530

    Update zphisher.sh

--- zphisher.sh
+++ zphisher.sh
@@ -1,13 +1,9 @@
-
 #!/bin/bash 
 
 ##   Zphisher : Automated Phishing Tool
 ##   Author : Abhinav Mukundh 
 ##   Version : 2.2
 ##   Github : https://github.com/AbhinavMukundhan
-
-
-
 
 ## ANSI colors (FG & BG)
 RED="$(printf '\033[31m')"  GREEN="$(printf '\033[32m')"  ORANGE="$(printf '\033[33m')"  BLUE="$(printf '\033[34m')"
